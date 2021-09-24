572d6dbe3501664fd3eed23d1d8ae6ea51f3ebf8
Bruno_Araújo <65301526+bruno19891989@users.noreply.github.com>
2021-09-23 21:26:35 -0300

    Add files via upload

---
+++ classificacao.js
@@ -0,0 +1,14 @@
+var quantidadedealuno = 14
+
+for (var controle = 0;controle < quantidadedealuno;controle++){
+    if(controle == 0){
+        console.log("o número é zero");
+    }
+
+    else if(controle % 2 == 0){
+        console.log("O número" + controle + " é par");
+    } else {
+        console.log(" O número" + controle + " é impar")
+    }
+    
+}
