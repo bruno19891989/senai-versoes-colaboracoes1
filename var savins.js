90c4d61f5a784aa2e01c51e85a62f0ddab123150
Bruno_Araújo <65301526+bruno19891989@users.noreply.github.com>
2021-11-04 21:25:48 -0300

    Add files via upload

---
+++ var savins.js
@@ -0,0 +1,25 @@
+var savins 
+
+if (data.length > data atual) {
+    console.log("permitir");
+} else { 
+    console.log("data invalida");
+ }
+
+ if ( < 18){
+     console.log(maiorque18  ": permitir cadastro")
+ } else{
+console.log(menorque18 ": não é permitido pela idade")
+
+ }
+}
+
+console.log("Participantes")
+
+var evento
+
+if (participantes >= 100){
+    console.log("permitir cadastro");
+} else{
+    console.log("cadastro não será permitido por ter excedido o limite")
+}
